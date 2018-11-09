177bcf52cd1c89b2af804075155820bafacc39b1
zjl <cquzjl@gmail.com>
2018-11-09 16:01:52 +0800

    delete unused code

--- src/main/java/cn/jimmiez/sample/demo/ShowPointCloud.java
+++ src/main/java/cn/jimmiez/sample/demo/ShowPointCloud.java
@@ -7,11 +7,6 @@ import java.io.File;
 
 public class ShowPointCloud {
     public static void main(String[] args) {
-        int a = 0;
-        a += 3;
-        //
-        BranchGroup bg = new BranchGroup();
-
         File f = new File(ShowPointCloud.class.getClassLoader().getResource("Dinosaur.ply").getFile());
         PointCloudFrame frame = new PointCloudFrame(f);
     }
